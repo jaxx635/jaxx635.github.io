58dc917d3b9a81a376408b2ecabf3d40b431f5d2
jaxx635 <57643390+jaxx635@users.noreply.github.com>
2020-07-28 00:45:28 -0400

    Update forecast.js

--- script/forecast.js
+++ script/forecast.js
@@ -2,7 +2,7 @@ const key ='4qZuGEurDhdoikpnDnaxKG8Hg2c5t6kc';
 
 //get location
 const getWeather = async (id)=>{
-    const base ='http://dataservice.accuweather.com/currentconditions/v1/';
+    const base ='https://dataservice.accuweather.com/currentconditions/v1/';
     const query=`${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -19,7 +19,7 @@ const getWeather = async (id)=>{
 // get city
 const getCity = async (city)=>{
     
-    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base ='https://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
